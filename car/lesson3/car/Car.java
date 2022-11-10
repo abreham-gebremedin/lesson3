189952fb356bcbc2fb5701012c386aea1d8134cd
abrehamhub <abrehamgebremedin12@gmail.com>
2022-11-10 10:53:52 +0300

    first commit

--- car/lesson3/car/Car.java
+++ car/lesson3/car/Car.java
@@ -14,19 +14,27 @@ public class Car{
 	public static void main(String[] args) {
 
 //		// Create an object e1 of class Employee (This will call the constructor 
-//		intialized the object and stored on the memory with 2010, "Toyota", "Camry", 15000 )
+//		initialized the object and stored on the memory with 2010, "Toyota", "Camry", 15000 )
 		Car myCar = new Car(2010, "Toyota", "Camry", 15000);
+		
+		//Assignment operator to assign an myCar reference to 
+		//myOtherCar reference variable then it will point to the same address location
 		Car myOtherCar = myCar;
+		
+//		//invokes the updatePrice() method with parameter 12000 to update mycar.price and myOtherCar.price with new price 12000 
 		myCar.updatePrice(12000);
+		
+//		print the value of mycar.price and myOtherCar.price which is 12000
+//		they have the same value because they point to the same reference
 		System.out.println(myCar.price);
 		System.out.println(myOtherCar.price);
 		
 	}
 
-
+	// Create a class constructor for the Car class with empty parameter we can call it default constructor
 	public Car(){
 	}
-	
+	// Create a class constructor for the Car class with int carYear, carMake,carModel and carPrice parameter
 	public Car(int carYear, String carMake, String carModel, double carPrice) {
 		super();
 		year = carYear;
@@ -37,19 +45,22 @@ public class Car{
 	
 	//update the price for the car
 	public void updatePrice(double newPrice){
+//		this line return updated price to the caller object.price
 		price = newPrice;
 	
 	}
-
+	  //declaring a method named staticMethod()
 	public static int staticMethod(){
 		return 5;
 	}
-
+	  //declaring a method named getYear()
 	public int getYear() {
+//		this line return updated year to the caller object.year
 		return year;
 	}
-
+	  //declaring a method named setYear((int carYear)) which has one parameter carYear
 	public void setYear(int carYear) {
+//		assign the value of local variable carYear to year
 		year = carYear;
 	}
 
